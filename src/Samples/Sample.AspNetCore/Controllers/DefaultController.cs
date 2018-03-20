dc7fcea115067c53cdc62bf60e795d4af16a5f9b
John Korsnes <john.korsnes@ice.net>
2018-03-20 12:01:22 +0100

    Added aspnet core sample

---
+++ src/Samples/Sample.AspNetCore/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PayEx.Client;
+using PayEx.Client.Models.Vipps.PaymentAPI.Common;
+using PayEx.Client.Models.Vipps.PaymentAPI.Request;
+
+namespace Sample.AspNetCore.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly PayExClient _client;
+
+        public DefaultController(PayExClient client)
+        {
+            _client = client;
+        }
+
+        [Route("/")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var prices = new Price
+            {
+                Amount = 10000,
+                VatAmount = 2500,
+                Type = PriceTypes.Vipps
+            };
+
+            var paymentRequest = new PaymentRequest("Sample.AspNetCore/1.0.0", "Some productname", "order123", "123456", prices);
+            var res = await _client.PostVippsPayment(paymentRequest);
+            return Ok($"Payment created with id : {res.Payment.Id}");
+        }
+    }
+}
---
+++ src/Samples/Sample.AspNetCore/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Options;
+using PayEx.Client;
+
+namespace Sample.AspNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<PayExOptions>(Configuration.GetSection("PayEx"));
+            services.AddTransient<IConfigureOptions<HttpClientFactoryOptions>, PayExClientConfigurator>();
+            services.AddHttpClient<PayExClient>("PayExClient");
+            services.AddMvc();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMvc();
+        }
+    }
+}
