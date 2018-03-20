57610c083e9fb14b0504bd8438e8b9559386b61a
John Korsnes <john.korsnes@ice.net>
2018-03-20 10:12:11 +0100

    Adds console sample

---
+++ src/Console.Sample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Options;
+using PayEx.Client;
+using PayEx.Client.Models.Vipps.PaymentAPI.Common;
+using PayEx.Client.Models.Vipps.PaymentAPI.Request;
+
+namespace Sample.ConsoleApp
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var payExOptions = new PayExOptions
+            {
+                ApiBaseUrl = new Uri("https://api.externalintegration.payex.com/"),
+                Token = "YOUR-PAYEX-ACCESS-TOKEN",
+                MerchantId = "YOUR-MERCHANT-ID",
+                MerchantName = "YOUR-MERCHANT-NAME",
+                CallBackUrl = new Uri("https://yourdomain.com/callbacks"),
+                CancelPageUrl = new Uri("https://yourdomain.com/cancel"),
+                CompletePageUrl = new Uri("https://yourdomain.com/complete")
+            };
+
+            IOptions<PayExOptions> options = new OptionsWrapper<PayExOptions>(payExOptions);
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {payExOptions.Token}");
+
+            var payExClient = new PayExClient(httpClient, options);
+            var prices = new Price
+            {
+                Amount = 10000,
+                VatAmount = 2500,
+                Type = PriceTypes.Vipps
+            };
+            var paymentRequest = new PaymentRequest("Console.Sample/1.0.0", "Some productname", "order123", "123456", prices);
+            var res = payExClient.PostVippsPayment(paymentRequest).GetAwaiter().GetResult();
+            Console.WriteLine($"Payment created with id : {res.Payment.Id}");
+        }
+    }
+}
---
+++ src/PayEx.Client/PriceTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace PayEx.Client
+{
+    public static class PriceTypes
+    {
+        public static string Vipps = "Vipps";
+        public static string Visa = "Visa";
+        public static string MasterCard = "MasterCard";
+    }
+}
