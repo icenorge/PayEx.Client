8a2878a6a98bef5811e6411597351423cda00f0a
Maxim <hardhub@users.noreply.github.com>
2019-05-07 08:47:20 +0300

    Make status in query optional.

--- src/PayEx.Client/Models/Vipps/PaymentAPI/Request/PaymentRequestExtensions.cs
+++ src/PayEx.Client/Models/Vipps/PaymentAPI/Request/PaymentRequestExtensions.cs
@@ -9,8 +9,8 @@ namespace PayEx.Client.Models.Vipps.PaymentAPI.Request
             payment.PayeeInfo.PayeeId = options.MerchantId;
             payment.PayeeInfo.PayeeName = options.MerchantName;
             payment.Urls.CallbackUrl = options.CallBackUrl?.ToString();
-            payment.Urls.CancelUrl = CallbackUrl(options.CancelPageUrl, "?status=cancel");
-            payment.Urls.CompleteUrl = CallbackUrl(options.CompletePageUrl, "?status=complete");
+            payment.Urls.CancelUrl = options.StatusInUrl ? CallbackUrl(options.CancelPageUrl, "?status=cancel") : options.CancelPageUrl.ToString();
+            payment.Urls.CompleteUrl = options.StatusInUrl ? CallbackUrl(options.CompletePageUrl, "?status=complete") : options.CompletePageUrl.ToString();
         }
 
         private static string CallbackUrl(Uri callbackBaseUrl, string relativePath)
--- src/PayEx.Client/PayExOptions.cs
+++ src/PayEx.Client/PayExOptions.cs
@@ -4,6 +4,11 @@ namespace PayEx.Client
 {
     public class PayExOptions
     {
+        public PayExOptions()
+        {
+            this.StatusInUrl = true;
+        }
+
         public Uri ApiBaseUrl { get; set; }
         public string Token { get; set; }
         public string MerchantId { get; set; }
@@ -11,6 +16,8 @@ namespace PayEx.Client
         public Uri CallBackUrl { get; set; }
         public Uri CancelPageUrl { get; set; }
         public Uri CompletePageUrl { get; set; }
+
+        public bool StatusInUrl { get; set; }
 
         public bool IsEmpty()
         {
